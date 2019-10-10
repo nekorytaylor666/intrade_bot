2852a413d88eba361934f9b5b33f875bfad25db5
nekorytaylor666 <art_gulba@mail.ru>
2019-10-10 10:16:18 +0900

    try to fix paths in linux

--- src/helpers/helpScenes/forCustomers.js
+++ src/helpers/helpScenes/forCustomers.js
@@ -17,7 +17,7 @@ forCustomers.enter(async ctx => {
       .extra(),
   );
   ctx.replyWithPhoto({
-    source: path.normalize('src\\static\\helpImageCustomers.jpeg'),
+    source: path.normalize('src/static/helpImageCustomers.jpeg'),
   });
 });
 
