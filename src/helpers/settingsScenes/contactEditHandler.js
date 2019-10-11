a4f83b2990fdb51b56a563a634d26b1ccc6e4730
nekorytaylor666 <art_gulba@mail.ru>
2019-10-11 16:04:11 +0900

    removing keyboard after message

--- src/helpers/settingsScenes/contactEditHandler.js
+++ src/helpers/settingsScenes/contactEditHandler.js
@@ -6,22 +6,28 @@ const User = require('../../models/User');
 const contactHandler = new Composer();
 
 contactHandler.hears('Имя', ctx => {
-  ctx.reply('Введите ваше имя!');
+  ctx.reply('Введите ваше имя!', Markup.removeKeyboard().extra());
   ctx.scene.session.inputType = 'NAME';
   ctx.wizard.next();
 });
 contactHandler.hears('Название компании', ctx => {
-  ctx.reply('Введите название вашей компании!');
+  ctx.reply(
+    'Введите название вашей компании!',
+    Markup.removeKeyboard().extra(),
+  );
   ctx.scene.session.inputType = 'COMPANY';
   ctx.wizard.next();
 });
 contactHandler.hears('Почта', ctx => {
-  ctx.reply('Введите ваш email!');
+  ctx.reply('Введите ваш email!', Markup.removeKeyboard().extra());
   ctx.scene.session.inputType = 'EMAIL';
   ctx.wizard.next();
 });
 contactHandler.hears('Телефон', ctx => {
-  ctx.reply('Введите ваш номер телефона!');
+  ctx.reply(
+    'Введите ваш номер телефона!',
+    Markup.removeKeyboard().extra(),
+  );
   ctx.scene.session.inputType = 'PHONE_NUMBER';
   ctx.wizard.next();
 });
